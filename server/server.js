c201421c24ae7fcc1f14f5a3694456f07d35c0db
Kedirech <brackcarmony@gmail.com>
2018-02-20 09:58:38 -0700

    Clear:

--- server/server.js
+++ server/server.js
@@ -7,39 +7,20 @@ const data = require('./data');
 const _ = require('lodash');
 
 app.use(bodyParser.json());
-app.use(session({
-  secret: 'All Your Secret Are Belong To Us',
-  resave: false,
-  saveUninitialized: false,
-  cookie: { secure: false }
-}))
+app.use( // Put session here
+
+)
 
 app.get('/api/set', (req, res)=>{
-  req.session.favorite = req.query.favorite || 'No Favorite';
 
-  res.send(`Thanks for registering your favorite as ${req.session.favorite}`);
 })
 
 app.get('/api/get', (req, res)=>{
-  res.send(req.session.favorite || 'You haven\'t set a Favorite');
+
 })
 
 app.get('/api/users', (req, res)=>{
-  if (req.query){
-    let filtered = data;
-    if (req.query.hair){
-      filtered = filtered.filter(e=>e.hairColor.includes(req.query.hair));
-    }
-    if (req.query.eye){
-      filtered = filtered.filter(e=>e.eyeColor.includes(req.query.eye));
-    }
-    if (req.query.name){
-      filtered = filtered.filter(e=>(e.name.first + ' ' + e.name.last).includes(req.query.name));
-    }
-    return res.send(filtered);
-  }else{
-    res.send(data);
-  }
+
 })
 
 
